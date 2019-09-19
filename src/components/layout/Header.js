420aa7691ee8bcea464de4e55946c34ba7d35fdb
Kevin <knphillips01@gmail.com>
2019-09-19 18:42:14 -0500

    completed todoapp with routing

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header style={headerStyle}>
       <h1>TodoList</h1>
+      <Link style={linkStyle} to='/'>Home</Link> | 
+      <Link style={linkStyle} to='/about'>About</Link>
     </header>
   )
 }
@@ -15,5 +18,9 @@ const headerStyle = {
   padding: '10px'
 }
 
+const linkStyle = {
+  color: '#fff',
+  textDecoration: 'none'
+}
 
 export default Header; 
